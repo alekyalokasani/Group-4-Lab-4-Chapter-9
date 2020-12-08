dc5d2a5e9ecb60932ae4255ccac72f961d659dd3
maneeshreddy22 <59578742+maneeshreddy22@users.noreply.github.com>
2020-12-07 23:44:34 -0500

    Updations
    Add click event handler, Run loop.

--- ch09-project3-start-master/js/chapter09-project03.js
+++ ch09-project3-start-master/js/chapter09-project03.js
@@ -30,11 +30,53 @@ spanEle.innerHTML = elements[i].tagName;
 
 elements[i].appendChild(spanEle);
 
+spanEle.addEventListener("click", function(e){
+
+
+    var parentElement = e.currentTarget.parentElement;
+    
+    var parentId = parentElement.getAttribute("id");
+    
+    var tagName = parentElement.tagName;
+    
+    var className = parentElement.className;
+    
+    var innerhtml = parentElement.innerHTML;
+    
+    var alertMsg = parentId + " " + tagName + " " + className + " " + innerhtml;
+    
+    alert(alertMsg);
+    
+    });
+
 
 
 }
 
 }
+document.getElementById("hide").addEventListener("click", function(){
+
+
+    while(document.getElementsByClassName("hoverNode").length > 0)
+    
+    {
+    
+    var spanNodes = document.getElementsByClassName("hoverNode");
+    
+    for (var i = 0; i < spanNodes.length; i++) {
+    
+    spanNodes[i].remove();
+    
+    }
+    
+    }
+    
+    document.getElementById("highlight").style.display = "block";
+    
+    document.getElementById("hide").style.display = "none";
+    
+    });
+
 
 
 document.getElementById("highlight").style.display = "none";
